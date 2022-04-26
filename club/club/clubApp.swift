72b3a3ae55e0f54e14798eaa003a4f6231a54dd8
KilloconQ <fjc1997v@gmail.com>
2022-04-26 18:14:43 -0500

    creando otra vez

---
+++ club/club/clubApp.swift
@@ -0,0 +1,17 @@
+//
+//  clubApp.swift
+//  club
+//
+//  Created by Fernando Corrales on 26/04/22.
+//
+
+import SwiftUI
+
+@main
+struct clubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
